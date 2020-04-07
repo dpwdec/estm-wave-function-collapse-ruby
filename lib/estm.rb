ee093e70ef155479e32d9c6ac2969cabfcf86b63
Declan <declankolakowski@gmail.com>
2020-04-07 20:54:09 +0000

    created valid_dirs input function

--- lib/estm.rb
+++ lib/estm.rb
@@ -2,23 +2,57 @@ require 'colorize'
 
 # Even simpler tiled model, implementation of wave function collapse in Ruby
 
+UP = [0, 1] # -->
+#>
+#|
+LEFT = [-1, 0]
+DOWN = [0, -1] # <--
+RIGHT = [1, 0]
+#|
+#>
+
 class Estm
   
   def initialize(input_matrix)
     @input_matrix = input_matrix
   end
   
+  # Uses the object's currently defined input matrix.
+  # weights --> 
+  # corresponds to how frequently a tile appears in the source matrix
+  #
+  # compatibilities -->
+  # contains a unique set of tile relationships in the format:
+  # [source_tile, related_tile, direction]
   def parse_example_matrix
     
     weights = Hash.new
     
-    @input_matrix.each_with_index do |row, x|
-      row.each_with_index do |cur_tile, y|
+    @input_matrix.each_with_index do |row, y|
+      row.each_with_index do |cur_tile, x|
          weights[cur_tile] = 0 if !weights.key?(cur_tile)
          weights[cur_tile] += 1
       end
     end
     
     return weights, Set.new
+  end
+  
+  # returns possible valid neighbour positions in the matrix
+  # dirs -->
+  # contains a set of 2D arrays containing offset translation vecotrs
+  # to valid neighbours
+  def valid_dirs(x, y)
+    width = @input_matrix.length
+    height = @input_matrix[0].length
+    
+    dirs = []
+    
+    dirs << LEFT if x > 0 
+    dirs << RIGHT if x < width - 1
+    dirs << DOWN if y > 0
+    dirs << UP if y < height - 1
+    
+    return dirs
   end
 end
--- spec/estm_spec.rb
+++ spec/estm_spec.rb
@@ -1,14 +1,19 @@
 require 'estm.rb'
 
 describe Estm do
-  describe "# parse_example_matrix" do
-    context "receives an input matrix of length 3 x 3" do
-        matrix = [
-        ['L', 'L', 'L'],
-        ['R', 'L', 'L'],
-        ['N', 'L', 'N']
+  context "receives an input matrix of length 4 x 7" do
+    matrix = [
+        ['L','L','L','L'],
+        ['L','L','L','L'],
+        ['L','L','L','L'],
+        ['L','C','C','L'],
+        ['C','S','S','C'],
+        ['S','S','S','S'],
+        ['S','S','S','S'],
         ]
-        subject = described_class.new(matrix)
+    subject = described_class.new(matrix)
+    
+    describe "# parse_example_matrix" do
       it "returns a Hash" do
         expect(subject.parse_example_matrix[0]).to be_a_kind_of(Hash)
       end
@@ -18,7 +23,49 @@ describe Estm do
       it "records 3 tile types" do
         expect(subject.parse_example_matrix[0].length).to eq(3)
       end
-      
     end
-  end
+  
+    describe "# valid_dirs" do
+      it "should accept (int, int) as its parameters" do
+        expect{ subject.valid_dirs(4, 7) }.to_not raise_error
+      end
+      
+      context "it receives top left edge coordinate" do
+        it "should return array [[1, 0], [0, -1]] for input 0, 0" do
+          expect(subject.valid_dirs(0, 0)).to contain_exactly(RIGHT, UP)
+        end
+      end
+      
+      context "it receives a top right edge coordinate" do 
+        it "should return [[-1, 0], [0, -1]] for input (0, 6)" do
+          expect(subject.valid_dirs(0, 3)).to contain_exactly(RIGHT, DOWN)
+        end
+      end
+      
+      context "it receives a bottom left edge coordinate" do
+        it "should return [[-1, 0], [0, 1]] for input (6, 0)" do
+          expect(subject.valid_dirs(6, 0)).to contain_exactly(LEFT, UP)
+        end
+      end
+      
+      context "it receives a bottom right edge coordinate" do
+        it "should return [[-1, 0], [0, -1]] for input (6, 3)" do
+          expect(subject.valid_dirs(6, 3)).to contain_exactly(LEFT, DOWN)
+        end
+      end
+      
+      context "it receives a middle coordinate" do
+        it "should return [[-1, 0], [0, -1], [1, 0], [0, 1]] for input (1, 1)" do
+          expect(subject.valid_dirs(1, 1)).to contain_exactly(RIGHT, LEFT, DOWN, UP)
+        end
+      end
+      
+      context "it receives a three edged coordinate" do
+        it "should return [[-1, 0], [0, -1], [0, 1]] for input (0, 1)" do
+          expect(subject.valid_dirs(0, 1)).to contain_exactly(RIGHT, DOWN, UP)
+        end
+      end
+    end
+    
+  end # input matrix context
 end
